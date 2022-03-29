6f1b090dc820c905e8d120f409d0bfe16a44be34
Maxon Tayebwa <maxtayebwa@gmail.com>
2022-03-29 11:33:48 +0300

    ability to update a movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,22 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    movie_params = params
+      .require(:movie)
+      .permit(:title, :rating, :description, :released_on, :total_gross)
+
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render :edit # render the edit template
+    end
+  end
 end
