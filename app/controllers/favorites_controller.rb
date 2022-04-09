b19d7f474c902668830a7e7bdc507e78d592e359
Maxon Tayebwa <maxtayebwa@gmail.com>
2022-04-09 20:59:58 +0300

    manage favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,22 @@
 class FavoritesController < ApplicationController
+  before_action :require_signin
+  before_action :set_movie, only: %i[create destroy]
+
+  def create
+    @movie.fans << current_user unless @movie.fans.include?(current_user)
+
+    redirect_to @movie, notice: "You favorited #{@movie.title}."
+  end
+
+  def destroy
+    @movie.fans.delete(current_user) if @movie.fans.include?(current_user)
+
+    redirect_to @movie, alert: "You unfavorited #{@movie.title}."
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  resources :favorites
-  root "movies#index"
+  root 'movies#index'
 
   resources :movies do
     resources :reviews
+    resources :favorites, only: %i[create destroy]
   end
 
   resources :users
-  get "signup" => "users#new"
+  get 'signup' => 'users#new'
 
-  resource :session, only: [:new, :create, :destroy]
-  get "signin" => "sessions#new"
+  resource :session, only: %i[new create destroy]
+  get 'signin' => 'sessions#new'
 end
